8abedf177ab25b7a99f617477f389d00a5b8a236
Kate <skoropeya@inbox.ru>
2021-11-10 00:11:55 +0300

    Отображение дополнительных комментариев по клику на кнопку

--- js/full-photo.js
+++ js/full-photo.js
@@ -1,24 +1,24 @@
-// import {createPosts} from './create-posts.js';
-// const PHOTOS_COUNT = 25;
-// const posts = Array.from({length: PHOTOS_COUNT}, createPosts);
 
 const bigPicture = document.querySelector('.big-picture');
+
 const image = bigPicture.querySelector('.big-picture__img img');
 const likesCount = bigPicture.querySelector('.likes-count');
-const commentsCount = bigPicture.querySelector('.comments-count');
+// const commentsCount = bigPicture.querySelector('.comments-count');
 const socialCaption = bigPicture.querySelector('.social__caption');
 const listComments = bigPicture.querySelector('.social__comments');
+
 const comment = listComments.querySelector('.social__comment');
 
-// const socialCommentCount = bigPicture.querySelector('.social__comment-count');
-// const commentsLoader = bigPicture.querySelector('.comments-loader');
+const commentCounter = bigPicture.querySelector('.social__comment-count');
+const commentsLoader = bigPicture.querySelector('.comments-loader');
 const buttonCancel = bigPicture.querySelector('#picture-cancel');
 
-const createComment = (post) => {
-  const listCommentsFragment = document.createDocumentFragment();
-  const allComments = post.comments;
+const COMMENTS_COUNT = 5;
 
-  allComments.forEach((commentItem) => {
+const createComments = (bitComments) => {
+  const listCommentsFragment = document.createDocumentFragment();
+
+  bitComments.forEach((commentItem) => {
     const newComment = comment.cloneNode(true);
     const img = newComment.querySelector('.social__picture');
     const text = newComment.querySelector('.social__text');
@@ -30,8 +30,42 @@ const createComment = (post) => {
     listCommentsFragment.appendChild(newComment);
   });
 
-  listComments.innerHTML = '';
   listComments.appendChild(listCommentsFragment);
+};
+
+const updateCounter = (counter, total) => {
+  commentCounter.innerHTML = `${counter} из <span class="comments-count">${total}</span> комментариев`;
+};
+
+const showComments = (post) => {
+  listComments.innerHTML = '';
+  const allComments = post.comments.slice();
+  let counter = 0;
+
+  if (allComments.length <= COMMENTS_COUNT) {
+
+    commentsLoader.classList.add('hidden');
+    counter = allComments.length;
+    createComments(allComments);
+
+  } else {
+    commentsLoader.classList.remove('hidden');
+    let bitComments;
+
+    bitComments = allComments.splice(0, COMMENTS_COUNT);
+    counter += bitComments.length;
+    createComments(bitComments);
+
+    const onCommentsLoaderClick = () => {
+      bitComments = allComments.splice(0, COMMENTS_COUNT);
+      counter += bitComments.length;
+      createComments(bitComments);
+      updateCounter(counter, post.comments.length);
+    };
+    commentsLoader.addEventListener('click', onCommentsLoaderClick);
+
+  }
+  updateCounter(counter, post.comments.length);
 };
 
 const closePhoto = () => {
@@ -42,6 +76,7 @@ const closePhoto = () => {
 const onButtonCancelClick = () => {
   closePhoto();
   buttonCancel.removeEventListener('click', onButtonCancelClick);
+  // commentsLoader.removeEventListener('click', onCommentsLoaderClick);
 };
 
 const openPhoto = (post) => {
@@ -50,22 +85,23 @@ const openPhoto = (post) => {
 
   image.src = post.url;
   likesCount.textContent = post.likes;
-  commentsCount.textContent = post.comments.length;
   socialCaption.textContent = post.description;
-  createComment(post);
+
+  showComments(post);
 
   const onEscKeydown = (evt) => {
     if (evt.key === 'Escape') {
       evt.preventDefault();
       closePhoto();
       buttonCancel.removeEventListener('click', onButtonCancelClick);
+      // commentsLoader.removeEventListener('click', onCommentsLoaderClick);
       document.removeEventListener('keydown', onEscKeydown);
     }
   };
 
   document.addEventListener('keydown', onEscKeydown);
   buttonCancel.addEventListener('click', onButtonCancelClick);
+  // commentsLoader.addEventListener('click', onCommentsLoaderClick);
 };
 
-// openPhoto(posts[0]);
 export {openPhoto};
