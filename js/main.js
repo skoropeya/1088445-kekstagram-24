2c13135dee4449809be1a2828d349cfad2eadb22
Kate <skoropeya@inbox.ru>
2021-11-13 23:05:27 +0300

    Добавлены фильтры

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 import { getData } from './api.js';
 import { showAlert } from './show-alert.js';
-import { showPreviews } from './gallery.js';
-import './full-photo.js';
+import { showPreviews, setPictureClick } from './gallery.js';
 import { onLoaderFileChange } from './upload-form.js';
+import { setFiltersClick } from './filters.js';
 
 const loaderFile = document.querySelector('#upload-file');
 
 document.addEventListener('DOMContentLoaded', () => {
   getData( (posts) => {
     showPreviews(posts);
-    // console.log(posts);
+    setPictureClick(posts);
+    setFiltersClick(posts);
   }, showAlert);
 });
 
