b08b000f1fe7186f861e53ff0ef70751ed4f3249
Kate <skoropeya@inbox.ru>
2021-11-14 17:10:59 +0300

    исправлен вывод комментариев к открытой фотографии

--- js/full-photo.js
+++ js/full-photo.js
@@ -1,74 +1,67 @@
-
 const bigPicture = document.querySelector('.big-picture');
-
 const image = bigPicture.querySelector('.big-picture__img img');
 const likesCount = bigPicture.querySelector('.likes-count');
-// const commentsCount = bigPicture.querySelector('.comments-count');
-const socialCaption = bigPicture.querySelector('.social__caption');
-const listComments = bigPicture.querySelector('.social__comments');
-
-const comment = listComments.querySelector('.social__comment');
-
+const descriptionImage = bigPicture.querySelector('.social__caption');
 const commentCounter = bigPicture.querySelector('.social__comment-count');
 const commentsLoader = bigPicture.querySelector('.comments-loader');
 const buttonCancel = bigPicture.querySelector('#picture-cancel');
+const listComments = bigPicture.querySelector('.social__comments');
+const comment = listComments.querySelector('.social__comment');
 
 const COMMENTS_COUNT = 5;
-
-const updateCounter = (counter, total) => {
-  commentCounter.innerHTML = `${counter} из <span class="comments-count">${total}</span> комментариев`;
-};
-
-const drawCommentItems = (bitComments) => {
-  const listCommentsFragment = document.createDocumentFragment();
-
-  bitComments.forEach((commentItem) => {
-    const newComment = comment.cloneNode(true);
-    const img = newComment.querySelector('.social__picture');
-    const text = newComment.querySelector('.social__text');
-
-    img.src = commentItem.avatar;
-    img.alt = commentItem.name;
-    text.textContent = commentItem.message;
-
-    listCommentsFragment.appendChild(newComment);
-  });
-
-  listComments.appendChild(listCommentsFragment);
-};
-
-const selectBitComments = (allComments) => {
-  const bitComments = allComments.splice(0, COMMENTS_COUNT);
-  let numberComments = 0;
-  if (bitComments) {
-    numberComments = bitComments.length;
-    drawCommentItems(bitComments);
-  }
-  return numberComments;
-};
-
-const onCommentsLoaderClick = (allComments, counter, total) => {
-  counter += selectBitComments(allComments);
-  updateCounter(counter, total);
-
-};
+let counter = 0;
 
 const showComments = (post) => {
-  listComments.innerHTML = '';
   const allComments = post.comments.slice();
   const total = post.comments.length;
-  let counter = 0;
+  let partComments = [];
+
+  const updateCounter = () => {
+    commentCounter.innerHTML = `${counter} из <span class="comments-count">${total}</span> комментариев`;
+  };
+
+  const drawComments = (comments) => {
+    const listCommentsFragment = document.createDocumentFragment();
+
+    comments.forEach((commentItem) => {
+      const newComment = comment.cloneNode(true);
+      const img = newComment.querySelector('.social__picture');
+      const text = newComment.querySelector('.social__text');
+
+      img.src = commentItem.avatar;
+      img.alt = commentItem.name;
+      text.textContent = commentItem.message;
+
+      listCommentsFragment.appendChild(newComment);
+    });
+
+    listComments.appendChild(listCommentsFragment);
+    updateCounter();
+  };
+
+  const selectPartComments = (comments) => {
+    partComments = comments.splice(0, COMMENTS_COUNT);
+    counter = counter + partComments.length;
+  };
+
+  const onCommentsLoaderClick = () => {
+    selectPartComments(allComments);
+    drawComments(partComments);
+    if (counter === total) {
+      commentsLoader.classList.add('hidden');
+    }
+  };
 
   if (total <= COMMENTS_COUNT) {
     counter = total;
+    drawComments(allComments);
     commentsLoader.classList.add('hidden');
-    drawCommentItems(allComments);
   } else {
-    counter = selectBitComments(allComments);
-    commentsLoader.addEventListener('click', () => onCommentsLoaderClick(allComments, counter, total));
-  }
+    selectPartComments(allComments);
+    drawComments(partComments);
 
-  updateCounter(counter, total);
+    commentsLoader.addEventListener('click', onCommentsLoaderClick);
+  }
 };
 
 const closePhoto = () => {
@@ -88,8 +81,9 @@ const openPhoto = (post) => {
 
   image.src = post.url;
   likesCount.textContent = post.likes;
-  socialCaption.textContent = post.description;
-
+  descriptionImage.textContent = post.description;
+  listComments.innerHTML = '';
+  counter = 0;
   showComments(post);
 
   const onEscKeydown = (evt) => {
